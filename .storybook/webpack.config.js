9a980c05e27d76430095e05f41e28b23b9f9a138
kecy <keishrio.oyama@linecorp.com>
2020-03-08 22:19:24 +0900

    add dependencies and config files to enable scss module

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,19 @@
-const path = require("path");
+const path = require('path');
 module.exports = ({ config }) => {
-  config.resolve.extensions.push(".ts", ".tsx");
-  config.module.rules.push({ test: /\.tsx?$/, loader: "ts-loader" })
+  config.resolve.extensions.push('.ts', '.tsx');
+  config.module.rules.push({ test: /\.tsx?$/, loader: 'ts-loader' })
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: [
+      'style-loader',
+      {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+        },
+      },
+      'sass-loader',
+    ]
+  });
   return config;
 };
