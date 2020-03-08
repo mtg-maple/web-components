a0c80b4a1c793aa0a4fdf6d5709be04373161438
kecy <keishrio.oyama@linecorp.com>
2020-03-09 07:45:04 +0900

    edit IconButton

--- src/atoms/IconButton/index.tsx
+++ src/atoms/IconButton/index.tsx
@@ -5,13 +5,40 @@ import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import styles from './style.scss';
 
 export interface IconButtonProps {
+  /** 
+   * IconDefinition (from '@fortawesome/fontawesome-svg-core')
+   * 
+   * Fontawesomeのアイコン定義を渡す
+   */
   icon: IconDefinition;
-  type: 'large' | 'regular' | 'small';
-  color: 'primary' | 'secondary' | 'text' | 'muteText'
+
+  /** 
+   * Size of the icon
+   * 
+   * アイコンの大きさを指定する
+   * @default 'regular'
+   */
+  type?: 'large' | 'regular' | 'small';
+
+  /**
+   * Color of the icon
+   *  
+   * アイコンの色を指定する 
+   * @default 'primary'
+  */
+  color?: 'primary' | 'secondary' | 'text' | 'muteText';
+
+  onClick?: (e: React.MouseEvent) => void;
 }
 
 const IconButton = (props: IconButtonProps) => (
-  <button className={`${styles.iconButton} ${styles[props.type]} ${styles[props.color]}`}>
+  <button
+    className={`${styles.iconButton} ${styles[props.type || 'regular']} ${styles[props.color || 'primary']}`}
+    onClick={(e) => {
+      if (props.onClick != undefined) {
+        props.onClick(e);
+      }
+    }}>
     <FontAwesomeIcon icon={props.icon}/>
   </button>
 );
