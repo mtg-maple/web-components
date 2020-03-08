26bfcf1758a7c0a7bacd28df13da3588be245e05
kecy <keishrio.oyama@linecorp.com>
2020-03-08 23:33:07 +0900

    modify webpack config to import global scss file

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir);
+}
+
 module.exports = ({ config }) => {
   config.resolve.extensions.push('.ts', '.tsx');
-  config.module.rules.push({ test: /\.tsx?$/, loader: 'ts-loader' })
+  config.module.rules.push({ test: /\.tsx?$/, loader: 'ts-loader' });
+
   config.module.rules.push({
     test: /\.scss$/,
+    exclude: [
+      resolve('src/styles'),
+    ],
     use: [
       'style-loader',
       {
@@ -12,8 +21,16 @@ module.exports = ({ config }) => {
           modules: true,
         },
       },
-      'sass-loader',
+      {
+        loader: 'sass-loader',
+        options: {
+          prependData: `
+            @import "${resolve('src/styles/_variables.scss')}";
+          `,
+        }
+      },
     ]
   });
+
   return config;
 };
