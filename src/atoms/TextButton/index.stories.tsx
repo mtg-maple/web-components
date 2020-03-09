83606aceba0b8e0875e4c5e42d3930bf64cda3da
kecy <keishrio.oyama@linecorp.com>
2020-03-09 09:09:07 +0900

    implement atoms/TextButton

--- src/atoms/TextButton/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from "@storybook/react";
-
-import TextButton from '../TextButton';
-
-const components = storiesOf("Atoms", module)
-components.add("TextButton", () => (
-  <TextButton label="Hello World"/>
-))
--- src/atoms/TextButton/index.tsx
+++ src/atoms/TextButton/index.tsx
@@ -1,11 +1,51 @@
 import React from 'react';
 
+import styles from './style.scss';
+
 export interface TextButtonProps {
+  /** 
+   * Label of the button
+  */
   label: string;
+
+  /**
+   * Color of the button
+   * @default 'primary'
+   */
+  color?: 'primary' | 'secondary';
+
+  /**
+   * Add style 'width: 100%'
+   * @default false
+   */
+  grow?: boolean;
+
+  /** 
+   * @default false
+  */
+ disabled?: boolean;
+
+  /**
+   * Handler of click events
+   */
+  onClick?: (e: React.MouseEvent) => void;
 }
 
 const TextButton = (props: TextButtonProps) => (
-  <button>{props.label}</button>
+  <button
+    className={`
+      ${styles.textButton} ${styles[props.color || 'primary']}
+      ${props.grow ? styles.grow : ''}
+    `}
+    disabled={props.disabled || false}
+    onClick={(e) => {
+      if (props.onClick != undefined) {
+        props.onClick(e);
+      }
+    }}
+  >
+    {props.label}
+  </button>
 );
 
 export default TextButton;
