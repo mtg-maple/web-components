774dbd5cca20bbc257eaad413f7c4ea9b3f8ddf6
kecy <keishrio.oyama@linecorp.com>
2020-03-13 22:50:46 +0900

    add atoms/Select component

---
+++ src/atoms/Select/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import styles from './style.scss';
+
+type SelectOption = {
+  label: string;
+  value: string;
+}
+
+type SelectProps = {
+  options: SelectOption[];
+
+  classNames?: string[];
+  
+  /**
+   * text alignment in select box
+   * 
+   * 'ltl': left, 'rtl': right
+   * @default 'auto'
+   */
+  dir?: 'auto' | 'ltl' | 'rtl';
+
+  onChange?: (e: React.ChangeEvent) => void;
+
+  onClick?: (e: React.MouseEvent) => void;
+}
+
+const Select = (props: SelectProps) => {
+  return (
+    <select className={[styles.select, props.classNames].join(' ')} dir={props.dir || 'auto'} onChange={props.onChange} onClick={props.onClick}>
+      {props.options.map((option: SelectOption) => (
+        <option value={option.value} key={`${option.value}-${option.label}`}>{option.label}</option>
+      ))}
+    </select>
+  )
+};
+
+export default Select;
