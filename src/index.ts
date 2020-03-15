97192cfed3a4cc5054c9562bca059bd582b9c73e
kecy <keishrio.oyama@linecorp.com>
2020-03-16 00:22:18 +0900

    transpile setting to publish the components to npm

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,45 @@
-import IconButton from './atoms/IconButton';
-import TextButton from './atoms/TextButton';
+/* Atoms */
 
-export { IconButton, TextButton };
+export { default as CardImage } from './atoms/CardImage';
+export * from './atoms/CardImage';
+
+export { default as Description } from './atoms/Description';
+export * from './atoms/Description';
+
+export { default as Heading } from './atoms/Heading';
+export * from './atoms/Heading';
+
+export { default as IconButton } from './atoms/IconButton';
+export * from './atoms/IconButton';
+
+export { default as Label } from './atoms/Label';
+export * from './atoms/Label';
+
+export { default as MagicSymbol } from './atoms/MagicSymbol';
+export * from './atoms/MagicSymbol';
+
+export { default as Number } from './atoms/Number';
+export * from './atoms/Number';
+
+export { default as Select } from './atoms/Select';
+export * from './atoms/Select';
+
+export { default as TextButton } from './atoms/TextButton';
+export * from './atoms/TextButton';
+
+export { default as UserIcon } from './atoms/UserIcon';
+export * from './atoms/UserIcon';
+
+
+/* Molecules */
+export { default as Input } from './molecules/Input';
+export * from './molecules/Input';
+
+export { default as ManaColors } from './molecules/ManaColors';
+export * from './molecules/ManaColors';
+
+export { default as ManaCost } from './molecules/ManaCost';
+export * from './molecules/ManaCost';
+
+export { default as Tags } from './molecules/Tags';
+export * from './molecules/Tags';
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    filename: 'index.js',
+    path: path.join(__dirname, 'dist'),
+    libraryTarget: "commonjs2"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          'ts-loader',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              prependData: `
+                @import "./src/properties.scss";
+              `,
+            }
+          },
+        ],
+      },
+      {
+        test: /\.(gif|png|jpg|eot|wof|woff|woff2|ttf|svg)$/,
+        loader: "url-loader"
+      },
+    ],
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+    ],
+    extensions: ['.ts', '.tsx', '.js', '.json', '.css', '.scss'],
+  },
+}
